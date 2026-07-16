305c0cce936fb9e47a5320d97b5733660873aea4
Tom Villani <thomas.villani@gmail.com>
2026-07-15 22:34:04 -0400

    Bump version: 0.1.0 → 0.1.0rc1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath("../src"))
 project = "localvectordb"
 copyright = "2025-2026, Tom Villani"
 author = "Tom Villani, Ph.D."
-release = "0.1.0"
+release = "0.1.0rc1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
